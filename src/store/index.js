2d2477797128e29982a29545b353b5297a6de5af
Juan <jucarmonam@unal.edu.co>
2020-12-07 12:00:27 -0500

    borrando archivo innecesarios

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,7 @@
 import Vue from "vue";
 import Vuex from "vuex";
 import axios from "axios";
-import loginDuenoModule from "./modules/loginDueno";
-import loginMedicoModule from "./modules/loginMedico";
 import userMenuBar from "./modules/userMenuBar";
-import perfilDuenoModule from "./modules/perfilDueno";
 
 axios.defaults.baseURL = "http://localhost:9000/";
 
@@ -15,9 +12,6 @@ export default new Vuex.Store({
   mutations: {},
   actions: {},
   modules: {
-    Dueno: loginDuenoModule,
-    Medico: loginMedicoModule,
     MenuBar: userMenuBar,
-    PerfilDueno: perfilDuenoModule,
   },
 });
